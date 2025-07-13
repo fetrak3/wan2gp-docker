e270f6626afa504307061913d2c700121927ce43
fetrak3 <fetrak3@outlook.com>
2025-07-13 22:38:05 +0200

    remove flash

--- start.sh
+++ start.sh
@@ -11,9 +11,8 @@ cd Wan2GP
 
 pip install --upgrade pip wheel
 pip install torch==2.6.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/test/cu124
-pip install -r requirements.txt
-pip install flash-attn==2.7.2.post1 jupyterlab
+pip install -r requirements.txt jupyterlab
 
 jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --NotebookApp.token="" --NotebookApp.password="" &
 
-python wgp.py --i2v --profile 3 --listen --server-port 7860 --attention flash
+python wgp.py --i2v --profile 3 --listen --server-port 7860
